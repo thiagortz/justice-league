712ee45066418c7e3ad60e59c8ae3cba40716725
Thiago Dias <thiago-dias@outlook.com>
2018-05-05 19:13:22 -0300

    adicionado versão da variavel de configuração.

--- src/app/risk_controller/blueprint.py
+++ src/app/risk_controller/blueprint.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from .controller import Risk
+from app.config import BLUEPRINT_VERSION
 
 
 class RiskBluePrint:
     def __init__(self):
-        blueprint = Blueprint('risk', __name__, url_prefix='/v1')
+        blueprint = Blueprint('risk', __name__, url_prefix=BLUEPRINT_VERSION)
         blueprint.add_url_rule('/risk/analysis/<cpf>', methods=['GET'], view_func=Risk().analysis)
         self.blueprint = blueprint
