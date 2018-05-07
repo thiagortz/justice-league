14c9157cfc269a84a478cd9d2467742f2bc7047e
Thiago Dias <thiago-dias@outlook.com>
2018-05-07 19:14:55 -0300

    Adicionado autenticação para executar pesquisas.

--- src/app/client_controller/controller.py
+++ src/app/client_controller/controller.py
@@ -1,7 +1,9 @@
 from flask_graphql import GraphQLView
+from flask_jwt import jwt_required
 
 
 class Client(GraphQLView):
 
+    @jwt_required()
     def get_middleware(self, request):
         return self.middleware
