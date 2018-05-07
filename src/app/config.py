2d09fc9752db58bc87afbe7d7f2970cbdf6ff92e
Thiago Dias <thiago-dias@outlook.com>
2018-05-07 19:14:20 -0300

    adicionado novas env e exemplos

--- src/app/config.py
+++ src/app/config.py
@@ -4,6 +4,10 @@ APP_NAME = 'JUSTICE_LEAGUE'
 PORT = int(os.environ.get('PORT', 5000))
 DEBUG_MODE = os.environ.get('DEBUG', False)
 BLUEPRINT_VERSION = '/v1'
+VERSION = '1.0.0'
+
+SECRET_KEY = os.environ.get('{}_SECRET_KEY', '7ghzgwkLGV3JTiJM5LJSz3YuS8zUCBjG')
+
 
 SUPER_MAN = {
     "url_api": os.getenv('{}_SUPER_MAN_URL'.format(APP_NAME))
